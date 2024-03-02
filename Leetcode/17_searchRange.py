03c8431f7500cb8327159320f8e2a3e8775376ae
Marcel Fernández Serrano <114417367+U198734@users.noreply.github.com>
2024-03-02 10:38:36 +0100

    Add files via upload

---
+++ Leetcode/17_searchRange.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def find_first(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+        
+        def find_last(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right
+        
+        first = find_first(nums, target)
+        last = find_last(nums, target)
+        
+        if first <= last:
+            return [first, last]
+        else:
+            return [-1, -1]
