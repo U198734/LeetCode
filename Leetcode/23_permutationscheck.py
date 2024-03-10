1ed17660033ffad4ae35a7236536640a2d204f14
Marcel Fernández Serrano <114417367+U198734@users.noreply.github.com>
2024-03-10 21:32:07 +0100

    Add files via upload

---
+++ Leetcode/23_permutationscheck.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # Find the first element that violates non-increasing order
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i >= 0:
+            # Find the smallest element to the right of nums[i] that is greater than nums[i]
+            j = len(nums) - 1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        # Reverse the sub-array from nums[i+1] to the end
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
